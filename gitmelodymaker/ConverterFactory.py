d34dba9b3c2523048cd073285f9a3095e48ff8f9
Ivan Gualandri <dreamos82@yahoo.it>
2021-01-03 01:12:14 +0000

    Add label for the improved converter

--- gitmelodymaker/ConverterFactory.py
+++ gitmelodymaker/ConverterFactory.py
@@ -3,4 +3,6 @@ class ConverterFactory:
     def get_converter(self, convertertype):
         if(convertertype == "mod"):
             return ModConverter()
+        elif (convertertype == "impmod":
+			return ImprovedModConverter()
 
